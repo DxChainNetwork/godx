13c66d0ee294e335e1386c8deeeffa91effcde36
Jacky Wang <jwang@dxchain.com>
2019-04-24 13:52:09 -0700

    Added cipher.go and implemented twofish-gcm algorithm

---
+++ crypto/cipher.go
@@ -0,0 +1,102 @@
+package crypto
+
+import (
+	"errors"
+	"github.com/DxChainNetwork/godx/crypto/twofishgcm"
+)
+
+const (
+	CipherCodeNotSupport uint8 = iota // Code not supported
+	PlainCipherCode                   // type code for plainCipherKey
+	GCMCipherCode                     // type code for gcmCipherKey
+)
+
+var (
+	ErrInvalidCipherType = errors.New("provided CipherType not supported")
+)
+
+// Key is the interface for cipher key, which is implemented by plainCipherKey, and gcmCipherKey
+type Key interface {
+	// Code return the code specified of the Key type
+	CodeName() string
+
+	// Overhead returns the overhead for decrypted text
+	Overhead() uint8
+
+	// Key return the key for the specified Key type
+	Key() []byte
+
+	// Encrypt will encrypt the input byte slice to cipher text
+	Encrypt([]byte) ([]byte, error)
+
+	// Decrypt will decrypt the input cipher text to plain text
+	Decrypt([]byte) ([]byte, error)
+
+	// DecryptInPlace will reuse the input memory and decrypt into the input byte slice.
+	// Note that the cipher text has greater length than plainText, so the plainText start
+	// at index of overhead
+	DecryptInPlace([]byte) ([]byte, error)
+}
+
+// plainCipherKey implements Key interface. Used only for tests and in scenario that no encryption is needed.
+type plainCipherKey struct{}
+
+// NewPlainCipherKey return a new plainCipherKey
+func NewPlainCipherKey() (*plainCipherKey, error) { return &plainCipherKey{}, nil }
+
+func (pc *plainCipherKey) CodeName() string { return "PlainText" } // Plaintext has code PlainCipherCode
+func (pc *plainCipherKey) Overhead() uint8  { return 0 }           // Plaintext has no overhead
+func (pc *plainCipherKey) Key() []byte      { return []byte{} }
+
+// Encrypt and Decript for plainCipherKey is simply return the input byte slice
+func (pc *plainCipherKey) Encrypt(plaintext []byte) ([]byte, error)         { return plaintext[:], nil }
+func (pc *plainCipherKey) Decrypt(cipherText []byte) ([]byte, error)        { return cipherText[:], nil }
+func (pc *plainCipherKey) DecryptInPlace(cipherText []byte) ([]byte, error) { return cipherText[:], nil }
+
+// NewCipherKey will create a Key using the key type specified by cipherCode, value with the input key
+func NewCipherKey(cipherCode uint8, key []byte) (Key, error) {
+	switch cipherCode {
+	case PlainCipherCode:
+		return &plainCipherKey{}, nil
+	case GCMCipherCode:
+		return twofishgcm.NewGCMCipherKey(key)
+	default:
+		return nil, ErrInvalidCipherType
+	}
+}
+
+// GenerateCipherKey generate a random seed and new a key according to the Key type specified by cipherCode
+func GenerateCipherKey(cipherCode uint8) (Key, error) {
+	switch cipherCode {
+	case PlainCipherCode:
+		return &plainCipherKey{}, nil
+	case GCMCipherCode:
+		return twofishgcm.GenerateGCMCipherKey()
+	default:
+		return nil, ErrInvalidCipherType
+	}
+}
+
+// Overhead return the size of the overhead for a cipher type specified by cipherCode
+func Overhead(cipherCode uint8) uint8 {
+	switch cipherCode {
+	case PlainCipherCode:
+		return (&plainCipherKey{}).Overhead()
+	case GCMCipherCode:
+		return (&(twofishgcm.GCMCipherKey{})).Overhead()
+	default:
+		return 0
+	}
+}
+
+// CodeByName returns the cipher code associated with the cipher name
+func CodeByName(cipherName string) uint8 {
+	switch cipherName {
+	case (&plainCipherKey{}).CodeName():
+		return PlainCipherCode
+	case (&(twofishgcm.GCMCipherKey{})).CodeName():
+		return GCMCipherCode
+	default:
+		return CipherCodeNotSupport
+	}
+}
---
+++ crypto/twofishgcm/gcmcipher.go
@@ -0,0 +1,116 @@
+package twofishgcm
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+const (
+	// GCMCipherKeyLength is the key length for GCMCipherKey
+	GCMCipherKeyLength = 32
+)
+
+// GCMCipherKey is the implementation of AES-GCM algorithm, implementing Key interface
+type GCMCipherKey [GCMCipherKeyLength]byte
+
+// Code return the GCMCipherCode specifying the key type
+func (gck *GCMCipherKey) CodeName() string {
+	return "TwoFish_GCM"
+}
+
+// Overhead returns the additional overhead used for nonce
+func (gck *GCMCipherKey) Overhead() uint8 {
+	return 28
+}
+
+// Key returns the encryption/decryption key for the GCMCipherKey
+func (gck *GCMCipherKey) Key() []byte {
+	key := make([]byte, GCMCipherKeyLength)
+	copy(key, gck[:])
+	return key
+}
+
+// Encrypt encrypt the input plainText using AES-GCM algorithm
+func (gck *GCMCipherKey) Encrypt(plainText []byte) ([]byte, error) {
+	gcm, err := gck.newGCM()
+	if err != nil {
+		return nil, err
+	}
+	// randomize the nonce
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	// seal the plainText using nonce
+	cipherText := gcm.Seal(nonce, nonce, plainText, nil)
+	return cipherText, nil
+}
+
+// Decrypt decrypt the input cipherText using AES-GCM algorithm
+func (gck *GCMCipherKey) Decrypt(cipherText []byte) ([]byte, error) {
+	gcm, err := gck.newGCM()
+	if err != nil {
+		return nil, nil
+	}
+	// First part of cipherText is nonce, and rest is cipherText
+	nonceSize := gcm.NonceSize()
+	if len(cipherText) < nonceSize {
+		return nil, fmt.Errorf("decrypt error: cipherText has length %v smaller than nonce %v", len(cipherText),
+			nonceSize)
+	}
+	nonce, cipherText := cipherText[:nonceSize], cipherText[nonceSize:]
+	// Decrypt
+	return gcm.Open(nil, nonce, cipherText, nil)
+}
+
+// DecryptInPlace make use of the input string and decrypt the cipherText in place
+func (gck *GCMCipherKey) DecryptInPlace(cipherText []byte) ([]byte, error) {
+	gcm, err := gck.newGCM()
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	if len(cipherText) < gcm.NonceSize() {
+		return nil, fmt.Errorf("decrypt error: cipherText has length %v smaller than nonce %v", len(cipherText),
+			nonceSize)
+	}
+	nonce, cipherText := cipherText[:nonceSize], cipherText[nonceSize:]
+	// Decrypt
+	return gcm.Open(cipherText[:0], nonce, cipherText, nil)
+}
+
+// newGCM is the internal method to create an AEAD using aes.NewCipher and cipher.NewGCM.
+// The method is used in encryption and decryption
+func (gck *GCMCipherKey) newGCM() (cipher.AEAD, error) {
+	c, err := aes.NewCipher(gck[:])
+	if err != nil {
+		return nil, err
+	}
+	return cipher.NewGCM(c)
+}
+
+// NewAESCipherKey returns a new GCMCipherKey using the input seed.
+// The input key must be of exact size of GCMCipherKeyLength, which is 32
+func NewGCMCipherKey(seed []byte) (*GCMCipherKey, error) {
+	if len(seed) != GCMCipherKeyLength {
+		err := fmt.Errorf("GCMCipherKey has unexpected length. Expect %v, Got %v", GCMCipherKeyLength, len(seed))
+		return nil, err
+	}
+
+	gck := &GCMCipherKey{}
+	copy(gck[:], seed)
+	return gck, nil
+}
+
+// generateGCMCipherKey will generate a new GCMCipherKey with random seed
+func GenerateGCMCipherKey() (*GCMCipherKey, error) {
+	seed := make([]byte, GCMCipherKeyLength)
+	_, err := rand.Read(seed)
+	if err != nil {
+		return nil, fmt.Errorf("cannot generate random seed")
+	}
+	return NewGCMCipherKey(seed)
+}
