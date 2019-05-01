2c54f7ce0c0adb487c9bc5976ec932108de68bab
mzhangdx <mzhang@dxchain.com>
2019-05-01 14:03:26 -0700

    changed the logging method

--- storage/storageclient/persist.go
+++ storage/storageclient/persist.go
@@ -25,19 +25,8 @@ func (sc *StorageClient) loadPersist() error {
 		return err
 	}
 
-	// initialize logger with multiple handlers (terminal and file)
+	// initialize logger
 	sc.log = log.New()
-	//logFileHandler, err := log.FileHandler(
-	//	filepath.Join(sc.persistDir, PersistLogname),
-	//	log.JSONFormatEx(false, true),
-	//)
-	//
-	//if err != nil {
-	//	log.Warn("failed to create logger file handler, logging information will not be saved")
-	//} else {
-	//	logStreamHandler := log.StreamHandler(os.Stdout, log.TerminalFormat(true))
-	//	sc.log.SetHandler(log.MultiHandler(logFileHandler, logStreamHandler))
-	//}
 
 	// TODO (mzhang): Create Write ahead logger
 
