b16d1fca6623135cffccf8c22c3fe7461271df19
Jacky Wang <jwang@dxchain.com>
2019-05-20 10:24:27 -0700

    refactored updates

--- storage/storageclient/filesystem/dxfile/update.go
+++ storage/storageclient/filesystem/dxfile/update.go
@@ -5,238 +5,29 @@
 package dxfile
 
 import (
-	"errors"
 	"fmt"
-	"os"
-
-	"github.com/DxChainNetwork/godx/common"
-	"github.com/DxChainNetwork/godx/common/writeaheadlog"
-	"github.com/DxChainNetwork/godx/rlp"
-)
-
-const (
-	// OpDeleteFile is the Operation name of a deleteUpdate
-	OpDeleteFile = "delete_file"
-
-	// OpInsertFile is the Operation name of an insertUpdate
-	OpInsertFile = "insert_file"
+	"github.com/DxChainNetwork/godx/storage"
 )
 
 // TODO: Refactor the apply update to write page updates. Each page has next Offset
 // 		 Each read will stop at next Offset == -1. Call it paged file.
 
-// dxfileUpdate defines the update interface for dxfile update.
-// Two types of update is supported: insertUpdate and deleteUpdate
-type dxfileUpdate interface {
-	apply() error                                    // Apply the content of the update
-	encodeToWalOp() (writeaheadlog.Operation, error) // convert an dxfileUpdate to writeaheadlog.Operation
-	fileName() string                                // filePath returns the filePath associated with the update
-}
-
-// insertUpdate defines an insert dxfile instruction, including filePath, Offset, and Data to write
-type (
-	insertUpdate struct {
-		Filename string
-		Offset   uint64
-		Data     []byte
-	}
-
-	// deleteUpdate defines a delete dxfile instruction, just the filePath to be deleted
-	deleteUpdate struct {
-		Filename string
-	}
-)
-
-// EncodeToWalOp convert an insertUpdate to wal.Operation
-func (iu *insertUpdate) encodeToWalOp() (writeaheadlog.Operation, error) {
-	data, err := rlp.EncodeToBytes(*iu)
-	if err != nil {
-		return writeaheadlog.Operation{}, err
-	}
-	return writeaheadlog.Operation{
-		Name: OpInsertFile,
-		Data: data,
-	}, err
-}
-
-// apply will open or create the file defined by iu.filePath, and write iu.Data at iu.Offset
-func (iu *insertUpdate) apply() error {
-	// open the file
-	f, err := os.OpenFile(iu.Filename, os.O_RDWR|os.O_CREATE, 0600)
-	if err != nil {
-		return fmt.Errorf("failed to apply insertUpdate: %v", err)
-	}
-	defer f.Close()
-	// write Data
-	if n, err := f.WriteAt(iu.Data, int64(iu.Offset)); err != nil {
-		return fmt.Errorf("failed to write insertUpdate Data: %v", err)
-	} else if n < len(iu.Data) {
-		return fmt.Errorf("failed to write full Data of an insertUpdate: %v", err)
-	}
-	return f.Sync()
-}
-
-func (iu *insertUpdate) fileName() string {
-	return iu.Filename
-}
-
-// encodeToWalOp will encode a deleteUpdate to writeaheadlog.Operation
-func (du *deleteUpdate) encodeToWalOp() (writeaheadlog.Operation, error) {
-	data, err := rlp.EncodeToBytes(*du)
-	if err != nil {
-		return writeaheadlog.Operation{}, err
-	}
-	return writeaheadlog.Operation{
-		Name: OpDeleteFile,
-		Data: data,
-	}, nil
-}
-
-// apply remove the file specified by deleteUpdate.filePath
-func (du *deleteUpdate) apply() error {
-	err := os.Remove(du.Filename)
-	if os.IsNotExist(err) {
-		return nil
-	}
-	return err
-}
-
-func (du *deleteUpdate) fileName() string {
-	return du.Filename
-}
-
-// decodeFromWalOp will decode the wal.Operation to a specified type of dxfileUpdate based on the op.Name field
-func decodeFromWalOp(op writeaheadlog.Operation) (dxfileUpdate, error) {
-	switch op.Name {
-	case OpInsertFile:
-		return decodeOpToInsertUpdate(op)
-	case OpDeleteFile:
-		return decodeOpToDeleteUpdate(op)
-	default:
-		return nil, fmt.Errorf("invalid op.Name: %v", op.Name)
-	}
-}
-
-// decodeOpToDeleteUpdate decode the wal.Operation to a deleteUpdate
-func decodeOpToDeleteUpdate(op writeaheadlog.Operation) (*deleteUpdate, error) {
-	var du deleteUpdate
-	err := rlp.DecodeBytes(op.Data, &du)
-	if err != nil {
-		return nil, err
-	}
-	return &du, nil
-}
-
-// decodeOpToInsertUpdate decode the op to an insertUpdate
-func decodeOpToInsertUpdate(op writeaheadlog.Operation) (*insertUpdate, error) {
-	var iu insertUpdate
-	err := rlp.DecodeBytes(op.Data, &iu)
-	if err != nil {
-		return nil, err
-	}
-	return &iu, nil
-}
-
 // createInsertUpdate create a DxFile insertion update.
-func (df *DxFile) createInsertUpdate(offset uint64, data []byte) (*insertUpdate, error) {
+func (df *DxFile) createInsertUpdate(offset uint64, data []byte) (*storage.InsertUpdate, error) {
 	// check validity of the Offset variable
 	if offset < 0 {
 		return nil, fmt.Errorf("file Offset cannot be negative: %d", offset)
 	}
-	return &insertUpdate{
-		Filename: df.filePath,
+	return &storage.InsertUpdate{
+		FileName: df.filePath,
 		Offset:   offset,
 		Data:     data,
 	}, nil
 }
 
 // createDeleteUpdate create a DxFile deletion update
-func (df *DxFile) createDeleteUpdate() (*deleteUpdate, error) {
-	return &deleteUpdate{
-		Filename: df.filePath,
+func (df *DxFile) createDeleteUpdate() (*storage.DeleteUpdate, error) {
+	return &storage.DeleteUpdate{
+		FileName: df.filePath,
 	}, nil
-}
-
-// applyUpdates use Wal to create a transaction to apply the updates
-func (df *DxFile) applyUpdates(updates []dxfileUpdate) error {
-	if df.deleted {
-		return errors.New("cannot apply updates on deleted file")
-	}
-	// first filter the unnecessary updates
-	updates = df.filterUpdates(updates)
-	ops, err := updatesToOps(updates)
-	if err != nil {
-		return fmt.Errorf("cannot apply updates: %v", err)
-	}
-	// Create the writeaheadlog transaction and apply till release
-	txn, err := df.wal.NewTransaction(ops)
-	if err != nil {
-		return fmt.Errorf("failed to create transaction: %v", err)
-	}
-	<-txn.InitComplete
-	if txn.InitErr != nil {
-		return fmt.Errorf("failed to create transaction: %v", err)
-	}
-	if err = <-txn.Commit(); err != nil {
-		return fmt.Errorf("failed to commit transaction: %v", err)
-	}
-	for _, u := range updates {
-		err := u.apply()
-		if err != nil {
-			return fmt.Errorf("failed to apply update: %v", err)
-		}
-	}
-	if err = txn.Release(); err != nil {
-		return fmt.Errorf("failed to release transaction: %v", err)
-	}
-	return nil
-}
-
-// filterUpdates filter the updates associated with the DxFile, and also filter all updates before a delete update
-func (df *DxFile) filterUpdates(updates []dxfileUpdate) []dxfileUpdate {
-	filtered := make([]dxfileUpdate, 0, len(updates))
-	for _, update := range updates {
-		if _, isInsertUpdate := update.(*insertUpdate); isInsertUpdate && update.fileName() != df.filePath {
-			continue
-		}
-		if _, isDeleteUpdate := update.(*deleteUpdate); isDeleteUpdate {
-			// If the update is deletion, remove all previous updates
-			filtered = filtered[:0]
-		}
-		filtered = append(filtered, update)
-	}
-	return filtered
-}
-
-// updatesToOps convert []dxfileUpdate to []wal.Operation
-func updatesToOps(updates []dxfileUpdate) ([]writeaheadlog.Operation, error) {
-	var fullErr error
-	ops := make([]writeaheadlog.Operation, len(updates))
-	for i, update := range updates {
-		op, err := update.encodeToWalOp()
-		if err != nil {
-			fullErr = common.ErrCompose(fullErr, err)
-			continue
-		}
-		ops[i] = op
-	}
-	return ops, fullErr
-}
-
-
-func IsDxFileOperation(op writeaheadlog.Operation) bool {
-	return op.Name == OpDeleteFile || op.Name == OpInsertFile
-}
-
-// ApplyOperations apply the operation of a dxfileUpdate
-func ApplyOperation(op writeaheadlog.Operation) error {
-	up, err := decodeFromWalOp(op)
-	if err != nil {
-		return fmt.Errorf("cannot decode operation: %v", err)
-	}
-	err = up.apply()
-	if err != nil {
-		return fmt.Errorf("cannot apply the update: %v", err)
-	}
-	return nil
 }
--- storage/storageclient/filesystem/dxfile/write.go
+++ storage/storageclient/filesystem/dxfile/write.go
@@ -7,6 +7,7 @@ package dxfile
 import (
 	"errors"
 	"fmt"
+	"github.com/DxChainNetwork/godx/storage"
 	"os"
 	"time"
 
@@ -19,7 +20,7 @@ func (df *DxFile) saveAll() error {
 	if df.deleted {
 		return errors.New("cannot save the file: file already deleted")
 	}
-	var updates []dxfileUpdate
+	var updates []storage.FileUpdate
 	// create updates for hostTable
 	up, hostTableSize, err := df.createHostTableUpdate()
 	if err != nil {
@@ -52,7 +53,7 @@ func (df *DxFile) saveAll() error {
 	updates = append(updates, up)
 
 	// save all updates
-	return df.applyUpdates(updates)
+	return storage.ApplyUpdates(df.wal, updates)
 }
 
 // rename create a series of transactions to rename the file to a new file
@@ -60,7 +61,7 @@ func (df *DxFile) rename(dxPath string, newFilePath string) error {
 	if df.deleted {
 		return errors.New("cannot rename the file: file already deleted")
 	}
-	var updates []dxfileUpdate
+	var updates []storage.FileUpdate
 	// create updates for delete
 	du, err := df.createDeleteUpdate()
 	if err != nil {
@@ -99,7 +100,7 @@ func (df *DxFile) rename(dxPath string, newFilePath string) error {
 	}
 	updates = append(updates, up)
 	// apply updates
-	return df.applyUpdates(updates)
+	return storage.ApplyUpdates(df.wal, updates)
 }
 
 // delete create and apply the deletion update
@@ -111,7 +112,7 @@ func (df *DxFile) delete() error {
 	if err != nil {
 		return fmt.Errorf("cannot create delete update: %v", err)
 	}
-	return df.applyUpdates([]dxfileUpdate{du})
+	return storage.ApplyUpdates(df.wal, []storage.FileUpdate{du})
 }
 
 // saveSegment save the Segment with the segmentIndex, and write to file
@@ -144,7 +145,7 @@ func (df *DxFile) saveSegments(indexes []int) error {
 	}
 	updates = append(updates, up)
 	// apply the updates
-	return df.applyUpdates(updates)
+	return storage.ApplyUpdates(df.wal, updates)
 }
 
 // saveHostTableUpdate save the host table as well as the metadata
@@ -156,7 +157,7 @@ func (df *DxFile) saveHostTableUpdate() error {
 	if err != nil {
 		return err
 	}
-	return df.applyUpdates(updates)
+	return storage.ApplyUpdates(df.wal, updates)
 }
 
 // saveMetadata only save the metadata
@@ -168,12 +169,12 @@ func (df *DxFile) saveMetadata() error {
 	if err != nil {
 		return err
 	}
-	return df.applyUpdates([]dxfileUpdate{up})
+	return storage.ApplyUpdates(df.wal, []storage.FileUpdate{up})
 }
 
 // createMetadataHostTableUpdate creates the update for metadata and hostTable
-func (df *DxFile) createMetadataHostTableUpdate() ([]dxfileUpdate, error) {
-	var updates []dxfileUpdate
+func (df *DxFile) createMetadataHostTableUpdate() ([]storage.FileUpdate, error) {
+	var updates []storage.FileUpdate
 	up, hostTableSize, err := df.createHostTableUpdate()
 	if err != nil {
 		return nil, err
@@ -199,7 +200,7 @@ func (df *DxFile) createMetadataHostTableUpdate() ([]dxfileUpdate, error) {
 
 // segmentShift shift Segment in persist file to the end of the persist file to give space for hostTable.
 // Return the corresponding update and the underlying error.
-func (df *DxFile) segmentShift(targetHostTableSize uint64) ([]dxfileUpdate, error) {
+func (df *DxFile) segmentShift(targetHostTableSize uint64) ([]storage.FileUpdate, error) {
 	f, err := os.OpenFile(df.filePath, os.O_RDONLY, 0777)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open the file %v: %v", df.filePath, err)
@@ -212,7 +213,7 @@ func (df *DxFile) segmentShift(targetHostTableSize uint64) ([]dxfileUpdate, erro
 	segmentSize := PageSize * segmentPersistNumPages(df.metadata.NumSectors)
 
 	// move the segment to the end of DxFile
-	var updates []dxfileUpdate
+	var updates []storage.FileUpdate
 	for i := 0; uint64(i) < numSegToShift; i++ {
 		seg, err := df.readSegment(f, prevOffset)
 		if err != nil {
@@ -253,7 +254,7 @@ func (df *DxFile) shiftOffset(targetHostTableSize uint64) (uint64, uint64, uint6
 }
 
 // createMetadataUpdate create an insert update for metadata
-func (df *DxFile) createMetadataUpdate() (dxfileUpdate, error) {
+func (df *DxFile) createMetadataUpdate() (storage.FileUpdate, error) {
 	df.metadata.TimeUpdate = unixNow()
 	metaBytes, err := rlp.EncodeToBytes(df.metadata)
 	if err != nil {
@@ -268,7 +269,7 @@ func (df *DxFile) createMetadataUpdate() (dxfileUpdate, error) {
 
 // createHostTableUpdate create a hostTable update. Return the insertUpdate, size of hostTable bytes
 // and the error
-func (df *DxFile) createHostTableUpdate() (dxfileUpdate, uint64, error) {
+func (df *DxFile) createHostTableUpdate() (storage.FileUpdate, uint64, error) {
 	df.metadata.TimeUpdate = unixNow()
 	hostTableBytes, err := rlp.EncodeToBytes(df.hostTable)
 	if err != nil {
@@ -282,7 +283,7 @@ func (df *DxFile) createHostTableUpdate() (dxfileUpdate, uint64, error) {
 }
 
 // createSegmentShiftUpdate create an Segment update
-func (df *DxFile) createSegmentUpdate(segmentIndex uint64, offset uint64) (dxfileUpdate, error) {
+func (df *DxFile) createSegmentUpdate(segmentIndex uint64, offset uint64) (storage.FileUpdate, error) {
 	df.metadata.TimeModify = unixNow()
 	if segmentIndex > uint64(len(df.segments)) {
 		return nil, fmt.Errorf("unexpected Index: %d", segmentIndex)
