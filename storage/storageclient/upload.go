0552a564d9d043876721d4e92d51ce666b9531b2
XiangZhang <xzhang@dxchain.com>
2019-06-26 21:36:30 +0800

    add some error log for testing in upload

--- storage/storageclient/upload.go
+++ storage/storageclient/upload.go
@@ -6,13 +6,14 @@ package storageclient
 
 import (
 	"fmt"
+	"math"
+	"os"
+
 	"github.com/DxChainNetwork/godx/crypto"
 	"github.com/DxChainNetwork/godx/storage"
 	"github.com/DxChainNetwork/godx/storage/storageclient/erasurecode"
 	"github.com/DxChainNetwork/godx/storage/storageclient/filesystem/dxdir"
 	"github.com/DxChainNetwork/godx/storage/storageclient/filesystem/dxfile"
-	"math"
-	"os"
 )
 
 // Upload instructs the storage client to start tracking a file. The storage client will
@@ -42,9 +43,11 @@ func (sc *StorageClient) Upload(up storage.FileUploadParams) error {
 
 	// Delete existing file if Override mode
 	if up.Mode == storage.Override {
-		if err := sc.DeleteFile(up.DxPath); err != nil && err != dxdir.ErrUnknownPath {
+		err := sc.DeleteFile(up.DxPath)
+		if err != nil && err != dxdir.ErrUnknownPath {
 			return fmt.Errorf("cannot to delete existing file, error: %v", err)
 		}
+		sc.log.Error("test error for DeleteFile in upload", "error", err)
 	}
 
 	// Setup ECTypeStandard's ErasureCode with default params
@@ -70,6 +73,7 @@ func (sc *StorageClient) Upload(up storage.FileUploadParams) error {
 			return err
 		}
 	}
+	sc.log.Error("test error for NewDxDir in upload", "error", err)
 
 	cipherKey, err := crypto.GenerateCipherKey(crypto.GCMCipherCode)
 	if err != nil {
@@ -86,6 +90,7 @@ func (sc *StorageClient) Upload(up storage.FileUploadParams) error {
 	}
 
 	if sourceInfo.Size() == 0 {
+		sc.log.Error("source file size is 0", "source", sourceInfo.Name())
 		return nil
 	}
 
@@ -105,5 +110,6 @@ func (sc *StorageClient) Upload(up storage.FileUploadParams) error {
 	case sc.uploadHeap.segmentComing <- struct{}{}:
 	default:
 	}
+	sc.log.Error("no error at end of upload")
 	return nil
 }
