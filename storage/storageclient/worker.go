52342e871f90ed79bef34147ec538d4ec73ddc75
Jacky Wang <jwang@dxchain.com>
2019-07-01 13:06:25 -0700

    Fixed disconnect usage. should be enodeid instead of string

--- storage/storageclient/worker.go
+++ storage/storageclient/worker.go
@@ -148,7 +148,7 @@ func (w *worker) killDownloading() {
 	if session != nil && ok {
 		log.Error("kill downloading, disconnected")
 		delete(w.client.sessionSet, contractID)
-		if err := w.client.disconnect(session, w.contract.EnodeID.String()); err != nil {
+		if err := w.client.disconnect(session, w.contract.EnodeID); err != nil {
 			w.client.log.Debug("can't close connection after downloading", "error", err)
 		}
 	}
@@ -247,7 +247,7 @@ func (w *worker) download(uds *unfinishedDownloadSegment) {
 
 		if session.LoadMaxUploadDownloadSectorNum() > MaxUploadDownloadSectorsNum {
 			delete(w.client.sessionSet, w.contract.ID)
-			if err := w.client.disconnect(session, w.contract.EnodeID.String()); err != nil {
+			if err := w.client.disconnect(session, w.contract.EnodeID); err != nil {
 				w.client.log.Error("close session failed", "err", err)
 			}
 		}
