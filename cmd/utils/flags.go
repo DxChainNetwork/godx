9ad931d6cc30216230f1d3b266d107e26b794e5a
Jacky Wang <jwang@dxchain.com>
2019-07-04 14:00:57 -0700

    Changed port number for both p2p, and rpc

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -602,7 +602,7 @@ var (
 	ListenPortFlag = cli.IntFlag{
 		Name:  "port",
 		Usage: "Network listening port",
-		Value: 30303,
+		Value: 36000,
 	}
 	BootnodesFlag = cli.StringFlag{
 		Name:  "bootnodes",
--- node/defaults.go
+++ node/defaults.go
@@ -29,9 +29,9 @@ import (
 
 const (
 	DefaultHTTPHost = "localhost" // Default host interface for the HTTP RPC server
-	DefaultHTTPPort = 8545        // Default TCP port for the HTTP RPC server
+	DefaultHTTPPort = 11688       // Default TCP port for the HTTP RPC server
 	DefaultWSHost   = "localhost" // Default host interface for the websocket RPC server
-	DefaultWSPort   = 8546        // Default TCP port for the websocket RPC server
+	DefaultWSPort   = 11689       // Default TCP port for the websocket RPC server
 )
 
 // DefaultConfig contains reasonable default settings.
@@ -44,7 +44,7 @@ var DefaultConfig = Config{
 	WSPort:           DefaultWSPort,
 	WSModules:        []string{"net", "web3"},
 	P2P: p2p.Config{
-		ListenAddr: ":30303",
+		ListenAddr: ":36000",
 		MaxPeers:   25,
 		NAT:        nat.Any(),
 	},
