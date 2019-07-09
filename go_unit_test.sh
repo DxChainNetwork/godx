12738f880630ba25f3eebfff436e2e1a5fb48c64
ewangdx <ewang@dxchain.com>
2019-07-09 14:41:02 -0700

    clean config.yml

--- go_unit_test.sh
+++ go_unit_test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "" > coverage.txt
 for module in $(go list ./... | grep -v vendor); do
-    go test -short -v -coverprofile=profile.out -covermode=count "$module"
+    go test -short -v -coverprofile=profile.out "$module"
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm -rf profile.out
