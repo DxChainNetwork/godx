2df6495929dfde1d5a81bebd36d177cf7ba2ce4b
XiangZhang <xzhang@dxchain.com>
2019-07-24 17:36:36 +0800

    TEST:add test case for dpos consensus funcs

---
+++ consensus/dpos/epoch_context_test.go
@@ -0,0 +1,37 @@
+// Copyright 2019 DxChain, All rights reserved.
+// Use of this source code is governed by an Apache
+// License 2.0 that can be found in the LICENSE file
+
+package dpos
+
+import (
+	"testing"
+
+	"github.com/DxChainNetwork/godx/common"
+	"github.com/DxChainNetwork/godx/core/types"
+	"github.com/DxChainNetwork/godx/ethdb"
+)
+
+func TestLookupValidator(t *testing.T) {
+	db := ethdb.NewMemDatabase()
+	dposCtx, _ := types.NewDposContext(db)
+	mockEpochContext := &EpochContext{
+		DposContext: dposCtx,
+	}
+	validators := []common.Address{
+		common.HexToAddress("0x1"),
+		common.HexToAddress("0x2"),
+		common.HexToAddress("0x3"),
+	}
+	mockEpochContext.DposContext.SetValidators(validators)
+	for i, expected := range validators {
+		got, _ := mockEpochContext.lookupValidator(int64(i) * blockInterval)
+		if got != expected {
+			t.Errorf("Failed to test lookup validator, %s was expected but got %s", expected.String(), got.String())
+		}
+	}
+	_, err := mockEpochContext.lookupValidator(blockInterval - 1)
+	if err != ErrInvalidMintBlockTime {
+		t.Errorf("Failed to test lookup validator. err '%v' was expected but got '%v'", ErrInvalidMintBlockTime, err)
+	}
+}
