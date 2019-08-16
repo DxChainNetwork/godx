79d372a7535e7a8608988379c931120286aa3a04
mzhangdx <mzhang@dxchain.com>
2019-08-16 11:41:27 -0700

    ADD: added fakeclientbackend

--- storage/fakeclientbackend.go
+++ storage/fakeclientbackend.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an Apache
 // License 2.0 that can be found in the LICENSE file
 
-package fakedata
+package storage
 
 import (
 	"context"
@@ -15,82 +15,81 @@ import (
 	"github.com/DxChainNetwork/godx/event"
 	"github.com/DxChainNetwork/godx/p2p/enode"
 	"github.com/DxChainNetwork/godx/params"
-	"github.com/DxChainNetwork/godx/storage"
 )
 
-type fakeClientBackend struct{}
+type FakeClientBackend struct{}
 
-func (fc *fakeClientBackend) Online() bool {
+func (fc *FakeClientBackend) Online() bool {
 	return true
 }
 
-func (fc *fakeClientBackend) Syncing() bool {
+func (fc *FakeClientBackend) Syncing() bool {
 	return false
 }
 
-func (fc *fakeClientBackend) GetStorageHostSetting(hostEnodeID enode.ID, hostEnodeURL string, config *storage.HostExtConfig) error {
-	config = &storage.HostExtConfig{
+func (fc *FakeClientBackend) GetStorageHostSetting(hostEnodeID enode.ID, hostEnodeURL string, config *HostExtConfig) error {
+	config = &HostExtConfig{
 		AcceptingContracts: true,
 	}
 	return nil
 }
 
-func (fc *fakeClientBackend) SubscribeChainChangeEvent(ch chan<- core.ChainChangeEvent) event.Subscription {
+func (fc *FakeClientBackend) SubscribeChainChangeEvent(ch chan<- core.ChainChangeEvent) event.Subscription {
 	return nil
 }
 
-func (fc *fakeClientBackend) GetTxByBlockHash(blockHash common.Hash) (types.Transactions, error) {
+func (fc *FakeClientBackend) GetTxByBlockHash(blockHash common.Hash) (types.Transactions, error) {
 	return types.Transactions{}, nil
 }
 
-func (fc *fakeClientBackend) SetupConnection(enodeURL string) (storage.Peer, error) {
+func (fc *FakeClientBackend) SetupConnection(enodeURL string) (Peer, error) {
 	return nil, nil
 }
 
-func (fc *fakeClientBackend) AccountManager() *accounts.Manager {
+func (fc *FakeClientBackend) AccountManager() *accounts.Manager {
 	return nil
 }
 
-func (fc *fakeClientBackend) ChainConfig() *params.ChainConfig {
+func (fc *FakeClientBackend) ChainConfig() *params.ChainConfig {
 	return nil
 }
 
-func (fc *fakeClientBackend) CurrentBlock() *types.Block {
+func (fc *FakeClientBackend) CurrentBlock() *types.Block {
 	return nil
 }
 
-func (fc *fakeClientBackend) SendTx(ctx context.Context, signedTx *types.Transaction) error {
+func (fc *FakeClientBackend) SendTx(ctx context.Context, signedTx *types.Transaction) error {
 	return nil
 }
 
-func (fc *fakeClientBackend) SuggestPrice(ctx context.Context) (*big.Int, error) {
+func (fc *FakeClientBackend) SuggestPrice(ctx context.Context) (*big.Int, error) {
 	return nil, nil
 }
 
-func (fc *fakeClientBackend) GetPoolNonce(ctx context.Context, addr common.Address) (uint64, error) {
+func (fc *FakeClientBackend) GetPoolNonce(ctx context.Context, addr common.Address) (uint64, error) {
 	return 0, nil
 }
 
-func (fc *fakeClientBackend) SendStorageContractCreateTx(clientAddr common.Address, input []byte) (common.Hash, error) {
+func (fc *FakeClientBackend) SendStorageContractCreateTx(clientAddr common.Address, input []byte) (common.Hash, error) {
 	return common.Hash{}, nil
 }
 
-func (fc *fakeClientBackend) GetHostAnnouncementWithBlockHash(blockHash common.Hash) (hostAnnouncements []types.HostAnnouncement, number uint64, errGet error) {
+func (fc *FakeClientBackend) GetHostAnnouncementWithBlockHash(blockHash common.Hash) (hostAnnouncements []types.HostAnnouncement, number uint64, errGet error) {
 	return nil, 0, nil
 }
 
-func (fc *fakeClientBackend) GetPaymentAddress() (common.Address, error) {
+func (fc *FakeClientBackend) GetPaymentAddress() (common.Address, error) {
 	return common.Address{}, nil
 }
 
-func (fc *fakeClientBackend) TryToRenewOrRevise(hostID enode.ID) bool {
+func (fc *FakeClientBackend) TryToRenewOrRevise(hostID enode.ID) bool {
 	return false
 }
 
-func (fc *fakeClientBackend) RevisionOrRenewingDone(hostID enode.ID) {}
+func (fc *FakeClientBackend) RevisionOrRenewingDone(hostID enode.ID) {}
 
-func (fc *fakeClientBackend) CheckAndUpdateConnection(peerNode *enode.Node) {}
+func (fc *FakeClientBackend) CheckAndUpdateConnection(peerNode *enode.Node) {}
 
-func (fc *fakeClientBackend) SelfEnodeURL() string {
+func (fc *FakeClientBackend) SelfEnodeURL() string {
 	return ""
 }
--- storage/storageclient/storagehostmanager/fakehostmanager.go
+++ storage/storageclient/storagehostmanager/fakehostmanager.go
@@ -1,1 +1,32 @@
+// Copyright 2019 DxChain, All rights reserved.
+// Use of this source code is governed by an Apache
+// License 2.0 that can be found in the LICENSE file
+
 package storagehostmanager
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DxChainNetwork/godx/storage"
+)
+
+const fakeHostManagerPersist = "testdata/fakehmpersist"
+
+// NewFakeHostManager create a fake StorageHostManager object used for testing purpose
+func NewFakeHostManager() (*StorageHostManager, error) {
+	// clear the previously saved data first
+	clearOldData()
+
+	// create and initialize new fake storage host  manager
+	fhm := New(fakeHostManagerPersist)
+	if err := fhm.Start(&storage.FakeClientBackend{}); err != nil {
+		return nil, fmt.Errorf("error start storage host manager: %s", err.Error())
+	}
+	return fhm, nil
+}
+
+// clearOldData will clear all data under the fakempersist directory
+func clearOldData() {
+	_ = os.RemoveAll("testdata/fakehmpersist/")
+}
