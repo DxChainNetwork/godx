1a77bba3beb57d4c27f83d69c7f1746596d67e1d
rabbitlxf <xfliu@dxchain.com>
2019-08-30 20:50:27 +0800

    ADD:add dpos block reward test case

--- consensus/dpos/dpos.go
+++ consensus/dpos/dpos.go
@@ -420,7 +420,7 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 	assignedReward := common.Big0
 
 	delegateTrie := dposContext.DelegateTrie()
-	delegatorIterator := trie.NewIterator(delegateTrie.PrefixIterator(header.Coinbase.Bytes()))
+	delegatorIterator := trie.NewIterator(delegateTrie.PrefixIterator(header.Validator.Bytes()))
 	for delegatorIterator.Next() {
 		delegator := common.BytesToAddress(delegatorIterator.Value)
 
