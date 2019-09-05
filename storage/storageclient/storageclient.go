dce6765460108e6d9e1517169bd270f9ffb039f6
xhuang <xhuang@dxchain.com>
2019-09-05 14:47:51 +0800

    FIX:the result of the to function may be empty, this is not what we want

--- storage/storageclient/storageclient.go
+++ storage/storageclient/storageclient.go
@@ -1048,6 +1048,9 @@ func (client *StorageClient) GetHostAnnouncementWithBlockHash(blockHash common.H
 	number = block.NumberU64()
 	txs := block.Transactions()
 	for _, tx := range txs {
+		if tx.To() == nil {
+			continue
+		}
 		p, ok := precompiled[*tx.To()]
 		if !ok {
 			continue
