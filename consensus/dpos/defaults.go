0ea7c062e8f92d1507b626e189a5b651297664db
mzhangdx <mzhang@dxchain.com>
2019-09-19 13:52:41 -0700

    MOD: modified the minDeposit and EpochInterval for testing purpose

--- consensus/dpos/defaults.go
+++ consensus/dpos/defaults.go
@@ -39,7 +39,7 @@ const (
 	BlockInterval = int64(10)
 
 	// EpochInterval indicates that a new epoch will be elected every a day
-	EpochInterval = int64(86400)
+	EpochInterval = int64(180)
 
 	// MaxVoteCount is the maximum number of candidates that a vote transaction could
 	// include
@@ -57,5 +57,5 @@ var (
 	constantinopleBlockReward = common.NewBigIntUint64(1e18).MultInt64(2)
 
 	// minDeposit defines the minimum deposit of candidate
-	minDeposit = common.NewBigIntUint64(1e18).MultInt64(1000)
+	minDeposit = common.NewBigIntUint64(1e18).MultInt64(1)
 )
