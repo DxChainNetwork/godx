af7130518e8495d72144eadefff26aeabd35cfdf
mzhangdx <mzhang@dxchain.com>
2019-09-18 17:01:56 -0700

    MOD: modified the candidateApply and vote tx api

--- internal/ethapi/precompiled_contract_tx_api.go
+++ internal/ethapi/precompiled_contract_tx_api.go
@@ -7,8 +7,8 @@ package ethapi
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/big"
-	"strconv"
 	"strings"
 
 	"github.com/DxChainNetwork/godx/accounts"
@@ -147,13 +147,13 @@ func NewPublicDposTxAPI(b Backend, nonceLock *AddrLocker) *PublicDposTxAPI {
 
 // SendApplyCandidateTx submit a apply candidate tx.
 // the parameter ratio is the award distribution ratio that candidate state.
-func (dpos *PublicDposTxAPI) SendApplyCandidateTx(fields map[string]interface{}) (common.Hash, error) {
+func (dpos *PublicDposTxAPI) SendApplyCandidateTx(fields map[string]string) (common.Hash, error) {
 	to := common.Address{}
 	to.SetBytes([]byte{13})
 	ctx := context.Background()
 
 	// parse precompile contract tx args
-	args, err := ParsePrecompileContractTxArgs(to, DposTxGas, fields)
+	args, err := ParseCandidateApplyTxArgs(to, DposTxGas, fields)
 	if err != nil {
 		return common.Hash{}, err
 	}
@@ -204,13 +204,13 @@ func (dpos *PublicDposTxAPI) SendCancelCandidateTx(from common.Address) (common.
 }
 
 // SendVoteTx submit a vote tx
-func (dpos *PublicDposTxAPI) SendVoteTx(fields map[string]interface{}) (common.Hash, error) {
+func (dpos *PublicDposTxAPI) SendVoteTx(fields map[string]string) (common.Hash, error) {
 	to := common.Address{}
 	to.SetBytes([]byte{15})
 	ctx := context.Background()
 
 	// parse precompile contract tx args
-	args, err := ParsePrecompileContractTxArgs(to, DposTxGas, fields)
+	args, err := ParseVoteTxArgs(to, DposTxGas, fields)
 	if err != nil {
 		return common.Hash{}, err
 	}
@@ -436,80 +436,158 @@ func CheckDposOperationTx(stateDB *state.StateDB, args *PrecompiledContractTxArg
 	}
 }
 
-// ParsePrecompileContractTxArgs parse the input fields to PrecompiledContractTxArgs format
-func ParsePrecompileContractTxArgs(to common.Address, gas uint64, fields map[string]interface{}) (args *PrecompiledContractTxArgs, err error) {
-	var data []byte
+func ParseCandidateApplyTxArgs(to common.Address, gas uint64, fields map[string]string) (*PrecompiledContractTxArgs, error) {
+	// parse the from field
 	var from common.Address
-	var deposit common.BigInt
-	for key, value := range fields {
-		switch key {
-		case "ratio":
-			str, ok := value.(string)
-			if !ok {
-				return nil, ErrRatioNotStringFormat
-			}
-
-			ratio, err := strconv.ParseUint(str, 10, 64)
-			if err != nil {
-				return nil, ErrParseStringToUint
-			}
-
-			if ratio > uint64(100) {
-				return nil, ErrInvalidAwardDistributionRatio
-			}
-
-			// convert uint8 to []byte
-			data = []byte{uint8(ratio)}
-
-		case "from":
-			str, ok := value.(string)
-			if !ok {
-				return nil, ErrFromNotStringFormat
-			}
-
-			from = common.HexToAddress(str)
-
-		case "deposit":
-			str, ok := value.(string)
-			if !ok {
-				return nil, ErrDepositNotStringFormat
-			}
-
-			// parse to big int,like "1000camel"、"10dx" and so on
-			deposit, err = unit.ParseCurrency(str)
-			if err != nil {
-				return nil, ErrParseStringToBigInt
-			}
-
-		case "candidates":
-			str, ok := value.(string)
-			if !ok {
-				return nil, ErrCandidatesNotStringFormat
-			}
-
-			hexAddrs := strings.Split(str, ",")
-
-			// limit the max vote count to 30
-			if len(hexAddrs) > MaxVoteCount {
-				return nil, ErrBeyondMaxVoteSize
-			}
-
-			candidates := make([]common.Address, 0)
-			for _, hexAddr := range hexAddrs {
-				addr := common.HexToAddress(hexAddr)
-				candidates = append(candidates, addr)
-			}
-
-			data, err = rlp.EncodeToBytes(candidates)
-			if err != nil {
-				return nil, ErrRLPEncodeCandidates
-			}
-
-		default:
-			return nil, ErrUnknownParameter
-		}
+	if fromStr, ok := fields["from"]; ok {
+		from = common.HexToAddress(fromStr)
 	}
 
-	args = NewPrecompiledContractTxArgs(from, to, data, deposit.BigIntPtr(), gas)
+	// form candidate tx data
+	data, err := formEncodedVoteTxData(fields)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewPrecompiledContractTxArgs(from, to, data, nil, gas), nil
+}
+
+func ParseVoteTxArgs(to common.Address, gas uint64, fields map[string]string) (*PrecompiledContractTxArgs, error) {
+	// parse the from field
+	var from common.Address
+	if fromStr, ok := fields["from"]; ok {
+		from = common.HexToAddress(fromStr)
+	}
+
+	// form candidate tx data
+	data, err := formEncodedAddCandidateTxData(fields)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewPrecompiledContractTxArgs(from, to, data, nil, gas), nil
+}
+
+func formEncodedVoteTxData(fields map[string]string) (data []byte, err error) {
+	// form the vote tx data
+	var voteTxData types.VoteTxData
+	if voteTxData, err = formVoteTxData(fields); err != nil {
+		return nil, err
+	}
+
+	// rlp encode data
+	return rlp.EncodeToBytes(voteTxData)
+}
+
+func formEncodedAddCandidateTxData(fields map[string]string) (data []byte, err error) {
+	// form the add candidate tx data
+	var addCandidateTxData types.AddCandidateTxData
+	if addCandidateTxData, err = formAddCandidateTxData(fields); err != nil {
+		return nil, err
+	}
+
+	// rlp encode data
+	return rlp.EncodeToBytes(addCandidateTxData)
+}
+
+func formVoteTxData(fields map[string]string) (data types.VoteTxData, err error) {
+	// get deposit
+	depositStr, ok := fields["deposit"]
+	if !ok {
+		return types.VoteTxData{}, fmt.Errorf("failed to form voteTxData, vote deposit is not provided")
+	}
+
+	// get candidates
+	candidatesStr, ok := fields["candidates"]
+	if !ok {
+		return types.VoteTxData{}, fmt.Errorf("failed to form voteTxData, vote candidates is not provided")
+	}
+
+	// parse candidates
+	if data.Candidates, err = parseCandidates(candidatesStr); err != nil {
+		return types.VoteTxData{}, err
+	}
+
+	// parse deposit
+	if data.Deposit, err = unit.ParseCurrency(depositStr); err != nil {
+		return types.VoteTxData{}, err
+	}
+
 	return
+}
+
+func formAddCandidateTxData(fields map[string]string) (data types.AddCandidateTxData, err error) {
+	// get reward ratio
+	rewardRatioStr, ok := fields["ratio"]
+	if !ok {
+		return types.AddCandidateTxData{}, fmt.Errorf("failed to form addCandidateTxData, candidate rewardRatio is not provided")
+	}
+
+	// get deposit
+	depositStr, ok := fields["deposit"]
+	if !ok {
+		return types.AddCandidateTxData{}, fmt.Errorf("failed to form addCandidateTxData, candidate deposit is not provided")
+	}
+
+	// parse reward ratio
+	if data.RewardRatio, err = parseRewardRatio(rewardRatioStr); err != nil {
+		return types.AddCandidateTxData{}, err
+	}
+
+	// parse deposit
+	if data.Deposit, err = unit.ParseCurrency(depositStr); err != nil {
+		return types.AddCandidateTxData{}, err
+	}
+
+	return
+}
+
+func parseCandidates(candidates string) ([]common.Address, error) {
+	// strip all white spaces
+	candidates = strings.Replace(candidates, " ", "", -1)
+
+	// convert it to a list of candidates
+	candidateAddresses := strings.Split(candidates, ",")
+	return candidatesValidationAndConversion(candidateAddresses)
+}
+
+// parseRewardRatio is used to convert the ratio from string to uint64
+// and to validate the parsed ratio
+func parseRewardRatio(ratio string) (uint64, error) {
+	// convert the string to uint64
+	rewardRatio, err := unit.ParseUint64(ratio, 1, "")
+	if err != nil {
+		return 0, ErrParseStringToUint
+	}
+
+	// validate the rewardRatio and return
+	return rewardRatioValidation(rewardRatio)
+}
+
+// rewardRatioValidation is used to validate the reward ratio
+func rewardRatioValidation(ratio uint64) (uint64, error) {
+	// check if the reward ratio
+	if ratio > dpos.RewardRatioDenominator {
+		return 0, ErrInvalidAwardDistributionRatio
+	}
+	return ratio, nil
+}
+
+// candidatesValidationAndConversion will check the string list format candidates first
+// and then convert the string list to address list
+func candidatesValidationAndConversion(candidates []string) ([]common.Address, error) {
+	// candidates validation
+	if len(candidates) > MaxVoteCount {
+		return nil, ErrBeyondMaxVoteSize
+	}
+
+	// candidates conversion
+	var candidateAddresses []common.Address
+	for _, candidate := range candidates {
+		addr := common.HexToAddress(candidate)
+		candidateAddresses = append(candidateAddresses, addr)
+	}
+
+	// return
+	return candidateAddresses, nil
 }
