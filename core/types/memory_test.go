a32cf6f54046c68d0d9fedd2851578f4657a28e0
Jacky Wang <jwang@dxchain.com>
2019-12-13 15:31:12 -0800

    ADD: reorganized structure and add value assertion in mem tests

--- core/types/memory_test.go
+++ core/types/memory_test.go
@@ -5,6 +5,8 @@
 package types
 
 import (
+	"bytes"
+	"fmt"
 	"hash"
 	"sync"
 	"testing"
@@ -13,6 +15,33 @@ import (
 )
 
 var testString = "ahsdhfkasjkdfkasdkfjkasdkfhakshfkasdlfjasdfhalksdjhfkljashdfjkashdfkaskdflasdkfhkasssdfkjashjdkfadkfkashdfkajskdhfkasdhjaslkhdflka"
+var shaRes = []byte{239, 243, 164, 0, 131, 103, 23, 137, 161, 71, 38, 30, 110, 160, 132, 208, 112, 170, 112, 222, 60, 144, 218, 120, 67, 232, 75, 102, 8, 139, 12, 140}
+
+func BenchmarkNewHasher_singleThread(b *testing.B)  { benchmarkHash(b, 1, newHashGetter{}) }
+func BenchmarkNewHasher_multiThread(b *testing.B)   { benchmarkHash(b, 3, newHashGetter{}) }
+func BenchmarkSyncHasher_singleThread(b *testing.B) { benchmarkHash(b, 1, newSyncHashGetter()) }
+func BenchmarkSyncHasher_multiThread(b *testing.B)  { benchmarkHash(b, 3, newSyncHashGetter()) }
+
+func benchmarkHash(b *testing.B, threads int, hg hashGetter) {
+	b.SetParallelism(threads)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			testHash(hg)
+		}
+	})
+}
+
+func testHash(hg hashGetter) {
+	h := hg.getHasher()
+	res := make([]byte, h.Size())
+	h.Write([]byte(testString))
+	h.Read(res)
+	hg.returnHasher(h)
+	if !bytes.Equal(res, shaRes) {
+		panic(fmt.Sprint("expect", shaRes, "got", res))
+	}
+	return
+}
 
 type hashGetter interface {
 	getHasher() sha
@@ -56,26 +85,4 @@ func (hg *syncHashGetter) getHasher() sha {
 func (hg *syncHashGetter) returnHasher(s sha) {
 	s.Reset()
 	hg.pool.Put(s)
-}
-
-func BenchmarkNewHasher_singleThread(b *testing.B)  { benchmarkHash(b, 1, newHashGetter{}) }
-func BenchmarkNewHasher_multiThread(b *testing.B)   { benchmarkHash(b, 3, newHashGetter{}) }
-func BenchmarkSyncHasher_singleThread(b *testing.B) { benchmarkHash(b, 1, newSyncHashGetter()) }
-func BenchmarkSyncHasher_multiThread(b *testing.B)  { benchmarkHash(b, 3, newSyncHashGetter()) }
-
-func benchmarkHash(b *testing.B, threads int, hg hashGetter) {
-	b.SetParallelism(threads)
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			testHash(hg)
-		}
-	})
-}
-
-func testHash(hg hashGetter) {
-	h := hg.getHasher()
-	res := make([]byte, h.Size())
-	h.Write([]byte(testString))
-	h.Read(res)
-	return
 }
