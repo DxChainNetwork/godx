26ef30a670a09efd389a54e3559998564490ebef
Jacky Wang <jwang@dxchain.com>
2020-01-27 11:40:53 -0800

    DEL: removed getDelegatorReward api and getValidatorReward api and corresponding unused functions

--- consensus/dpos/state.go
+++ consensus/dpos/state.go
@@ -58,12 +58,6 @@ var (
 	// KeySumAllocatedReward is the key of sum allocated block reward until now
 	KeySumAllocatedReward = common.BytesToHash([]byte("sum-allocated-reward"))
 
-	// KeyValidatorAllocatedReward is the key of reward allocated to validator at last block
-	KeyValidatorAllocatedReward = common.BytesToHash([]byte("validator-allocated-reward-last-block"))
-
-	// KeyDelegatorAllocatedReward is the key of reward allocated to delegator at last block
-	KeyDelegatorAllocatedReward = common.BytesToHash([]byte("delegator-allocated-reward-last-block"))
-
 	// KeyCandidateDepositLastEpoch is the candidate deposit in the last epoch
 	KeyCandidateDepositLastEpoch = common.BytesToHash([]byte("candidate-deposit-last-epoch"))
 )
@@ -264,30 +258,6 @@ func addSumAllocatedReward(state stateDB, diff common.BigInt) {
 	prevSum := getSumAllocatedReward(state)
 	newSum := prevSum.Add(diff)
 	setSumAllocatedReward(state, newSum)
-}
-
-// SetValidatorAllocatedReward set the current allocated reward for validator
-func SetValidatorAllocatedReward(state stateDB, reward common.BigInt, validator common.Address) {
-	v := common.BigToHash(reward.BigIntPtr())
-	state.SetState(validator, KeyValidatorAllocatedReward, v)
-}
-
-// GetValidatorAllocatedReward get the current allocated reward for validator
-func GetValidatorAllocatedReward(state stateDB, validator common.Address) *big.Int {
-	v := state.GetState(validator, KeyValidatorAllocatedReward)
-	return v.Big()
-}
-
-// SetDelegatorAllocatedReward set the current allocated reward for delegator
-func SetDelegatorAllocatedReward(state stateDB, reward common.BigInt, delegator common.Address) {
-	v := common.BigToHash(reward.BigIntPtr())
-	state.SetState(delegator, KeyDelegatorAllocatedReward, v)
-}
-
-// GetDelegatorAllocatedReward get the current allocated reward for delegator
-func GetDelegatorAllocatedReward(state stateDB, delegator common.Address) *big.Int {
-	v := state.GetState(delegator, KeyDelegatorAllocatedReward)
-	return v.Big()
 }
 
 // GetCandidateDepositLastEpoch get the candidates deposit at last epoch
